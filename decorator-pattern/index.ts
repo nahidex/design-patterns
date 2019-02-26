225df3cc1338ad908335ea2854774c8ebb6d6a89
Habibullah Nahid <nahidex@gmail.com>
2019-02-27 00:01:09 +0600

    Solve coffee size issue

--- decorator-pattern/index.ts
+++ decorator-pattern/index.ts
@@ -1,8 +1,21 @@
 abstract class Beverage {
     description:string = "Unknown Beverage";
+    static SMALL:number = 1;
+    static LARGE:number = 2;
+    static EXTREA_LARGE:number = 3;
+    
+    size:number;
 
     getDescription():string {
         return this.description;
+    }
+
+    setSize(size: number):void {
+        this.size = size;
+    }
+
+    getSize():number {
+        return this.size;
     }
 
     abstract cost():number;
@@ -45,7 +58,7 @@ class Mocha extends CondimentDecorator {
     getDescription(): string {
         return this.beverage.getDescription() + ", Mocha";
     }
-
+    
     cost(): number {
         return 0.20 + this.beverage.cost();
     }
@@ -70,6 +83,38 @@ class Whip extends CondimentDecorator {
 }
 
 
+
+class Soy extends CondimentDecorator {
+    beverage:Beverage;
+
+    constructor(beverage: Beverage) {
+        super();
+        this.beverage = beverage;
+    }
+
+    getDescription(): string {
+        return this.beverage.getDescription() + ", Soy";
+    }
+    
+    cost(): number {
+
+        let cost:number = this.beverage.cost();
+        const size:number = this.beverage.getSize();
+
+        if(size === Beverage.SMALL) {
+            cost += 0.10;
+        } else if(size === Beverage.LARGE) {
+            cost += 0.15;
+        } else if(size === Beverage.EXTREA_LARGE) {
+            cost += 0.20;
+        }
+
+        return cost;
+    }
+}
+
+
+
 const beverage:Beverage = new Expresso();
 console.log(beverage.getDescription() + " $" + beverage.cost());
 
@@ -77,17 +122,13 @@ let beverage1:Beverage;
 
 beverage1 = new HouseBlend();
 beverage1 = new Mocha(beverage1);
-beverage1 = new Mocha(beverage1);
-beverage1 = new Mocha(beverage1);
-beverage1 = new Mocha(beverage1);
-beverage1 = new Mocha(beverage1);
-beverage1 = new Mocha(beverage1);
 beverage1 = new Whip(beverage1);
 beverage1 = new Whip(beverage1);
-beverage1 = new Whip(beverage1);
-beverage1 = new Whip(beverage1);
-beverage1 = new Whip(beverage1);
-beverage1 = new Whip(beverage1);
-
 console.log(beverage1.getDescription() + " $" + beverage1.cost());
+
+let beverage2:Beverage = new Expresso();
+beverage2.setSize(1);
+// beverage2 = new Mocha(beverage2);
+beverage2 = new Soy(beverage2);
+console.log(beverage2.getDescription() + " $" + beverage2.cost());
 
